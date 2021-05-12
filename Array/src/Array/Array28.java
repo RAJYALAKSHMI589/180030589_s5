ba8510229b781b13e56d5421b410081aa1934b1e
RAJI JANNAVARAPU <RAJI JANNAVARAPU@LAPTOP-EK6QK9Q3>
2021-05-12 14:10:28 +0530

    milestone1

---
+++ Array/src/Array/Array28.java
@@ -0,0 +1,19 @@
+package Array;
+import java.util.*;
+public class Array28 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of elements ");
+    int n = sc.nextInt();
+    int[] a =new int[n];
+    int i;
+    for(i=0;i<n;i++) 
+    {
+      a[i]=sc.nextInt();
+    }
+  }
+
+
+
+}
